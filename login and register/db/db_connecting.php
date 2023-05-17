2ada60d23a915138a13982767221d87b6cade136
Andrew Adel <60392594+Andrew-Adel@users.noreply.github.com>
2023-05-17 03:09:04 +0300

    Update db_connecting.php

--- login and register/db/db_connecting.php
+++ login and register/db/db_connecting.php
@@ -3,12 +3,36 @@
     $uname = "root";
     $password = "";
 
-    $db_name = "test_db";
-    $connection = mysqli_connect($sname,$uname,$password,$db_name);
+    $db1_name = "db3";
+    $connectionDB1 = mysqli_connect($sname,$uname,$password,$db1_name);
 
-    if (!$connection){
+    $db2_name = "db4";
+    $connectionDB2 = mysqli_connect($sname,$uname,$password,$db2_name);
+    if (!$connectionDB1 || !$connectionDB2){
         echo "Connection Failed!";
     }
     else
-        // echo "Connection is Success :)";
+        echo "Connection is Success to db1 :)";
+
+    // $connectionBuyer = mysqli_connect($sname,$uname,$password,"distributed2");
+    // if (!$connectionBuyer){
+    //     echo "Connection Failed!";
+    // }
+    // else
+    //     echo "Connection is Success to db2 :)";
+
+    // $connection_Basic_info = mysqli_connect($sname,$uname,$password,"seller_info");
+
+
+    // $sql = "SELECT * FROM seller ";
+    // $result = mysqli_query($connectionSeller, $sql);
+    // echo "<br>";
+    // print_r($result);
+    // echo "<br>";
+    // $sq2 = "SELECT * FROM buyer";
+    // $result2 = mysqli_query($connectionBuyer, $sq2);
+    // print_r($result2);
+
+
+    
 ?>
